1e9362dc84f953978ca21045f1833e83d2fa758f
Neil Shevlin <neilshevlin.98@gmail.com>
2022-09-23 15:23:11 +0100

    Added worker handling of server request

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -4,7 +4,7 @@ use std::{env, str};
 
 fn main(){
 //    setup a worker server that takes a request from the server and respods to it
-    let socket = UdpSocket::bind("127.0.0.1:4444").expect("Could not bind socket");
+    let socket = UdpSocket::bind("0.0.0.0:8080").expect("Could not bind socket");
 
     loop {
         let mut buf = [0u8; 1500];
@@ -13,11 +13,23 @@ fn main(){
         match socket.recv_from(&mut buf){
             Ok((amt, src)) => {
                 thread::spawn(move || {
-                    println!("handling conneciton from {}", src);
-                    sock.send_to(&buf[..amt], &src).expect("Failed to send data");
+                    println!("Recieved: {}", str::from_utf8(&buf).expect("Could not write buffer as string"));
+                    sock.send_to(b"THIS WORKER HAS ACKNOWLEDGED YOUR REQUEST", src).expect("Failed to send data");
                 });
             }
             Err(e) => println!("Unable to recieve datagram: {}", e)
         }
     }
 }
+
+async fn handle_work_request(socket: UdpSocket, src: SocketAddr, buf: &[u8], amt: usize){
+
+    socket.send_to(b"THIS WORKER HAS ACKNOWLEDGED YOUR REQUEST", src).expect("Failed to send data");
+
+    // wait five seconds to simulate a delay
+    thread::sleep(std::time::Duration::from_secs(5));
+
+    socket.send_to(b"THIS WORKER HAS COMPLETED YOUR REQUEST", src).expect("Failed to send data");
+
+}
+
