50faf1ab020698e314886cac3c777e566e5ffd2a
Neil Shevlin <neilshevlin.98@gmail.com>
2022-09-30 20:59:18 +0100

    Added tokio to client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,21 +1,76 @@
-use std::net::UdpSocket;
-use std::{str, io};
-//CLIENT
-fn main(){
-    let socket = UdpSocket::bind("0.0.0.0:8000").expect("Could not bind socket");
+use std::env;
+use std::error::Error;
+use std::io::{stdin, Read};
+use std::net::SocketAddr;
+use tokio::net::UdpSocket;
+use std::{io};
 
-    socket.connect("0.0.0.0:8080").expect("Could not connect to server");
+struct Client {
+    socket: UdpSocket,
+    buf: Vec<u8>,
+    to_send: Option<(usize, SocketAddr)>,
+}
+impl Client {
+    async fn run(self) -> Result<(), io::Error> {
+        let Client {
+            socket,
+            buf: _,
+            to_send,
+        } = self;
 
-    loop {
-        let mut input = String::new();
-        let mut buffer = [0u8; 1500];
-        match io::stdin().read_line(&mut input){
-            Ok(_) => {
-                socket.send(input.as_bytes()).expect("Failed to send data");
-                socket.recv_from(&mut buffer).expect("Failed to recieve data");
-                println!("Response from server: {}", str::from_utf8(&buffer).expect("Could not write buffer as string"));
+        loop {
+            if let Some((_size, _peer)) = to_send {
+                let mut data = vec![0; 1350];
+                let len = socket.recv(&mut data).await?;
+                println!(
+                    "Received {} bytes:\n{}",
+                    len,
+                    String::from_utf8_lossy(&data[..len])
+                );
             }
-            Err(e) => println!("Unable to read line: {}", e)
+
+            let mut input  = String::new();
+            io::stdin().read_line(&mut input)?;
+            socket.send(input.as_bytes()).await?;
+            let mut data = vec![0; 1350];
+            let len = socket.recv(&mut data).await?;
+            println!(
+                "Received {} bytes:{}",
+                len,
+                String::from_utf8_lossy(&data[..len])
+            );
         }
     }
 }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let _remote_addr: SocketAddr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".into())
+        .parse()?;
+
+    // We use port 0 to let the operating system allocate an available port for us.
+    let _local_addr: SocketAddr = env::args()
+        .nth(2)
+        .unwrap_or_else(|| "127.0.0.1:8000".into())
+        .parse()?;
+
+    let socket = UdpSocket::bind(_local_addr).await?;
+    println!("Listening on: {}", socket.local_addr()?);
+    const MAX_DATAGRAM_SIZE: usize = 65_507;
+    socket.connect("127.0.0.1:8080").await?;
+
+    let client = Client {
+        socket,
+        buf: vec![0; 1350],
+        to_send: None,
+    };
+
+    client.run().await?;
+    Ok(())
+}
+
+
+
+
+
