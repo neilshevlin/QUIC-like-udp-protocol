732431a350e1c59fe524f093aa5d1e8a3e3648d1
Neil Shevlin <neilshevlin.98@gmail.com>
2022-09-30 13:13:36 +0100

    Add client input updates

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,14 +4,18 @@ use std::{str, io};
 fn main(){
     let socket = UdpSocket::bind("0.0.0.0:8000").expect("Could not bind socket");
 
-    socket.connect("0.0.0.0:8888").expect("Could not connect to server");
+    socket.connect("0.0.0.0:8080").expect("Could not connect to server");
 
     loop {
         let mut input = String::new();
         let mut buffer = [0u8; 1500];
-        io::stdin().read_line(&mut input).expect("Failed to read from stdin");
-        socket.send(input.as_bytes()).expect("Failed to send data");
-        socket.recv_from(&mut buffer).expect("Failed to recieve data");
-        println!("Recieved: {}", str::from_utf8(&buffer).expect("Could not write buffer as string"));
+        match io::stdin().read_line(&mut input){
+            Ok(_) => {
+                socket.send(input.as_bytes()).expect("Failed to send data");
+                socket.recv_from(&mut buffer).expect("Failed to recieve data");
+                println!("Response from server: {}", str::from_utf8(&buffer).expect("Could not write buffer as string"));
+            }
+            Err(e) => println!("Unable to read line: {}", e)
+        }
     }
 }
